0724dc93d326ec0a4f736a4a15c03b38b342b798
cortesjpb <48661799+cortesjpb@users.noreply.github.com>
2019-03-17 23:08:50 -0300

    Template para el notebook
    Es el template que vamos a usar en las competencias

---
+++ templateAsado.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i,a,b) for(int i=(a); i<(b);i++)
+#define dfor(i,a,b) for(int i=(a);i>=(b);i--)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) dfor(i,n,0)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
+#define dbg(v) cout << #v" = "<<v<<endl//;)
+typedef long long ll;
+typedef pair<int,int> ii;
+const int maxn = 100010;
+
+	
+int main() {
+    //std::ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+ 
+    return 0;
+}
